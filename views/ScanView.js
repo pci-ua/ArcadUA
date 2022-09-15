f349e290098bac9b4e3174059608c68458f2af48
Mathieu T <mathieu.toulon@free.fr>
2022-09-15 17:28:03 +0200

    📷 Fix and improvments for camera

--- views/ScanView.js
+++ views/ScanView.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import SokobanService from '../services/SokobanService.js';
 import GameView from './GameView.js';
+import { Dimensions} from 'react-native'
 
 class ScanView extends Component {
 
@@ -17,10 +18,17 @@ class ScanView extends Component {
 	render() {
 		return ( 
 			<QRCodeScanner
-				onRead={this.scanned.bind(this)} cameraStyle={{  height: 300 }}
-
+				onRead={this.scanned.bind(this)}
+				fadeIn={true}
+				reactivateTimeout={400}
 				topContent={<View><Text>Trouvez un stand avec un QRCode !</Text></View>}
 				bottomContent={<View></View>}
+				showMarker={true}
+				customMarker={<Image source={require('../assets/QRCodeFrame.png')} style={{}} /> }
+				notAuthorizedView={<View><Text style={{color: 'red'}}>Veuillez donner les droits pour la camera dans les paramètres de votre appareil.</Text></View>}
+
+				cameraContainerStyle={{height:Dimensions.get('window').height-40}}
+				cameraStyle={{height:Dimensions.get('window').height-40}}
 			/> 
 		);
 	}
